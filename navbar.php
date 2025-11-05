a24a6fea0afa59a41e9a84e156988748abb16b48
Chandilene <chandileneborges@gmail.com>
2025-11-05 13:50:33 -0300

    feat: navbar css

--- navbar.php
+++ navbar.php
@@ -1,8 +1,8 @@
-<header>
+<header >
 
-    <nav class="navbar navbar-dark bg-dark">
+    <nav class="navbar navbar-dark " style="background-color: rgb(67 112 157);">
         <div class="container-md">
-        <!-- rgb(67 112 157) -->
+      
             <a class="navbar-brand" href="index.php">
                 <img src="assets/logo_dourado.png"
                     alt="ALPHA Suplementos Logo"
@@ -13,9 +13,9 @@
             <?php if (isset($_SESSION['logado']) && $_SESSION['logado'] === true): ?>
 
                 <a href="logout.php"
-                    class="btn btn-outline-light btn-sm ms-auto"
+                    class="btn btn-sm ms-auto bg-danger text-white"
                     title="Sair do Sistema">
-                    <span class="bi bi-box-arrow-left "></span>&nbsp;
+                    <span class="bi bi-box-arrow-left text-white"></span>&nbsp;
                     Sair
                 </a>
 
